a9f74b35eb3461ad022b2fae44e27e289e7b5023
Stefan Priebsch <stefan@priebsch.de>
2011-10-17 23:35:07 -0700

    Whitespace.

--- demo/AuditLog.php
+++ demo/AuditLog.php
@@ -10,5 +10,4 @@ $bob = new Person('Bob');
 $bob->acceptAuditLogger(new AuditLogger());
 
 $bob->setEmail('bob@example.com', new DateTime('2011-10-01 12:00:00'));
-
 $bob->setEmail('bob@bobsdomain.com');
